c2dc0fa3a4b12383c6a938a5e2be1d21534ef84f
Dayse Soares <daysedesign@gmail.com>
2019-11-07 14:16:58 -0300

    Projeto JS

---
+++ WS_JSConfirm/Confirm.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Confirm.aspx.cs" Inherits="Confirm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+<head runat="server">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <script type="text/javascript">
+        function myTestFunction()
+        {
+            if (confirm('Do you want to proceed ?'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" ID="Button1" Text="Button" OnClick="Button1_Click1" />
+            <br /><br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WS_JSConfirm/Confirm2.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Text;
+
+public partial class Confirm2 : System.Web.UI.Page
+{
+
+    bool _isConfirmNeeded = true;
+    string _confirmMessage = string.Empty;
+
+    public bool IsConfirmNeeded
+    {
+        get { return _isConfirmNeeded; }
+        set { _isConfirmNeeded = value; }
+    }
+
+    public string ConfirmMessage
+    {
+        get { return _confirmMessage; }
+        set { _confirmMessage = value; }
+    }
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //Set Properties
+        IsConfirmNeeded = true;
+        ConfirmMessage = "Do you want to proceed ?";
+
+        // Insure that the __doPostBack() JavaScript is added to the page...
+        ClientScript.GetPostBackEventReference(this, string.Empty);
+
+        if (IsPostBack)
+        {
+            string eventTarget = Request["__EVENTTARGET"] ?? string.Empty;
+            string eventArgument = Request["__EVENTARGUMENT"] ?? string.Empty;
+
+            switch (eventTarget)
+            {
+                case "UserConfirmationPostBack":
+                    if (Convert.ToBoolean(eventArgument))
+                    {
+                        // User said to go ahead and do it...
+                        Label1.Text = "Mensagem 01";
+                    }
+                    else
+                    {
+                        Label1.Text = "Mensagem 02";
+                        // User said NOT to do it...
+                    }
+                    break;
+            }
+        }
+
+    }
+
+    protected void Button1_OnClick(object sender, EventArgs e)
+    {
+        if (IsConfirmNeeded)
+        {
+            StringBuilder javaScript = new StringBuilder();
+
+            string scriptKey = "ConfirmationScript";
+
+            javaScript.AppendFormat("var userConfirmation = window.confirm('{0}');\n", ConfirmMessage);
+
+            // Un-comment to only PostBack if user answers OK...
+            //javaScript.Append("if ( userConfirmation == true )\n");
+            javaScript.Append("__doPostBack('UserConfirmationPostBack', userConfirmation);\n");
+
+            ClientScript.RegisterStartupScript(GetType(), scriptKey, javaScript.ToString(), true);
+        }
+    }
+}
